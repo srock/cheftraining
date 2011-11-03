5527843ca04c7e9a7ac70a6dfb66c5c0013e811f
Shane Rock <shane@rightscale.com>
2011-11-03 21:01:25 +0000

    yarr we be addin\' your metadatas

--- cookbooks/apache-quickstart/metadata.rb
+++ cookbooks/apache-quickstart/metadata.rb
@@ -6,3 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 recipe "apache-quickstart::default", "This be the captain of the pirate ship, matey"
+
+attribute "quickstart/name",
+:display_name => "Your name",
+:description => "Yarr this be where you put in your name",
+:required => true,
+:recipes => [ "apache-quickstart::default" ]
